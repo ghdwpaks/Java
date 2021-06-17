9ec8c9abf66f08dedf3ef23273fd848b24ea6a3b
ghdwpaks <hjm7904@gmail.com>
2021-06-18 00:33:16 +0900

    EXTENDS(상속) 공부 2 , static in extends

--- study_subjects/extends/extends/src/extend/main.java
+++ study_subjects/extends/extends/src/extend/main.java
@@ -4,11 +4,22 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Studentdeveloper ghdwpaks = new Studentdeveloper();
+		ghdwpaks.dev();
+		ghdwpaks.stack++;
+		System.out.println("ghdwpaks.stack : "+ghdwpaks.stack);
+		person ghd1 = new person();
+		System.out.println(ghd1.stack);
+		ghd1.stack++;
+		System.out.println(ghd1.stack);
+		
+		
 	}
 
 }
+
 class person {
+	static int stack = 0;
 	void eat() {
 		System.out.println("π‰¿ª ∏‘Ω¿¥œ¥Ÿ");
 	}
